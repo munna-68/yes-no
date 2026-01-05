152d4dbab4a042e7f177f175681256ea57b145ca
munna-68 <mahamudmunna68@gmail.com>
2026-01-05 11:43:43 +0600

    adding functionality to the no button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const yesBtn = document.querySelector('.yes')
+const noBtn = document.querySelector('.no')
+const container = document.querySelector('.container')
+const popUpModal = document.querySelector('.pop-up-modal')
+const emojiHolder = document.querySelector('.emoji-holder')
+
+noBtn.addEventListener('click', () => {
+  container.classList.add('no')
+  popUpModal.classList.add('hidden')
+  emojiHolder.classList.remove('hidden')
+})
+
